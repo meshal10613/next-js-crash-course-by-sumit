7af0d84831f677504bb48c3261e966b5b701ac30
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-08-03 21:33:17 +0600

    learn with sumit github created

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    // throw new Error("There was a huge error");
+    return (
+        <main className="mt-10">
+            <div>This is About page</div>
+        </main>
+    )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,10 +19,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="p-6">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+          <ul className="flex gap-5">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/blog">Blog</Link></li>
+          </ul>
+        </nav>
+        <hr/>
         {children}
       </body>
     </html>
